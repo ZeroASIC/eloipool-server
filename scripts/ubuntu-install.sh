013d9e1471b109ddd446b665bbf584321b2400bd
ZeroASIC <zeroasic@gmail.com>
2014-02-06 00:28:05 -0800

    Update ubuntu-install.sh
    Patched from twanschik.

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -4,13 +4,13 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing Supporting Files."
-sudo apt-get install python-software-properties
+sudo apt-get -y install python-software-properties
 wait
 sudo add-apt-repository ppa:bitcoin/bitcoin
 wait
 sudo apt-get update
 wait
-sudo apt-get install bitcoind python3 python3-dev python3.2-dev python-requests build-essential git
+sudo apt-get -y install bitcoind python3 python3-dev python3.2-dev python-requests build-essential git
 wait
 
 $DIR/installpool.sh
